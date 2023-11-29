e39421b2c40599a9081f91a73c40f3c91d9a5153
Brian-CodeNation <brian.harkins@wearecodenation.com>
2023-11-29 09:42:24 +0000

    Added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [sum, setSum] = useState("");
+  
+  const buttons = [
+    "0", "1", "2", "3", "4", "5", 
+    "6", "7", "8", "9", "+", "-", 
+    "x", "÷", "C", "=" ];
+
+  const handleClick = (btnPress) => {
+    // work out the sum
+    // update the stateFunction setSum
+    setSum(sum + btnPress);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc">
+      <h1>Calculator Demo</h1>
+      <h3 className="display">SUM: {sum}</h3>
+      <div>
+        {buttons.map((button, index) => {
+          return (
+            <button key={index} className="buttonBox" onClick={()=>handleClick(button)}>{button}</button>
+          )
+        })}
+      </div>
     </div>
   );
 }
