8165d4d788f274d84f2b5f2b6f4f936894b14e8f
Brian-CodeNation <brian.harkins@wearecodenation.com>
2023-11-29 10:20:41 +0000

    Added update display and calculate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 const App = () => {
 
@@ -8,12 +9,20 @@ const App = () => {
   const buttons = [
     "0", "1", "2", "3", "4", "5", 
     "6", "7", "8", "9", "+", "-", 
-    "x", "÷", "C", "=" ];
+    "*", "/", "C", "=" ];
 
   const handleClick = (btnPress) => {
-    // work out the sum
-    // update the stateFunction setSum
-    setSum(sum + btnPress);
+    
+    console.log(sum)
+    if (btnPress === "=") {
+      let expression = sum;
+      setSum(evaluate(expression))
+    } else if (btnPress === "C") {
+      setSum("");
+    } else {
+      setSum(sum + btnPress);
+    };
+
   };
 
   return (
